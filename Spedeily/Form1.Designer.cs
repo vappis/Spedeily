233b20ccc6b18742cec62ce00281d0bf3aef447c
Mikko Elopuro <vappis@gmail.com>
2015-12-26 00:42:37 +0200

    First commit!

---
+++ Spedeily/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Spedeily
+{
+    partial class Spedeily
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.redButton = new System.Windows.Forms.Button();
+            this.yellowButton = new System.Windows.Forms.Button();
+            this.greenButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.startButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.redButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.yellowButton, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.greenButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1217, 640);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.DarkRed;
+            this.redButton.Location = new System.Drawing.Point(3, 3);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(399, 506);
+            this.redButton.TabIndex = 0;
+            this.redButton.UseVisualStyleBackColor = false;
+            this.redButton.Click += new System.EventHandler(this.redButton_Click);
+            // 
+            // yellowButton
+            // 
+            this.yellowButton.BackColor = System.Drawing.Color.Gold;
+            this.yellowButton.Location = new System.Drawing.Point(408, 3);
+            this.yellowButton.Name = "yellowButton";
+            this.yellowButton.Size = new System.Drawing.Size(399, 506);
+            this.yellowButton.TabIndex = 1;
+            this.yellowButton.UseVisualStyleBackColor = false;
+            this.yellowButton.Click += new System.EventHandler(this.yellowButton_Click);
+            // 
+            // greenButton
+            // 
+            this.greenButton.BackColor = System.Drawing.Color.DarkGreen;
+            this.greenButton.Location = new System.Drawing.Point(813, 3);
+            this.greenButton.Name = "greenButton";
+            this.greenButton.Size = new System.Drawing.Size(401, 506);
+            this.greenButton.TabIndex = 2;
+            this.greenButton.UseVisualStyleBackColor = false;
+            this.greenButton.Click += new System.EventHandler(this.greenButton_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 3);
+            this.flowLayoutPanel1.Controls.Add(this.startButton);
+            this.flowLayoutPanel1.Controls.Add(this.closeButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 515);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1211, 122);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(3, 3);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(582, 110);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.closeButton.Location = new System.Drawing.Point(591, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.closeButton.Size = new System.Drawing.Size(144, 110);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Spedeily
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 640);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Spedeily";
+            this.ShowIcon = false;
+            this.Text = "Spedeily";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button redButton;
+        private System.Windows.Forms.Button yellowButton;
+        private System.Windows.Forms.Button greenButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ Spedeily/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Spedeily
+{
+    public partial class Spedeily : Form
+    {
+        private List<int> litButton = new List<int>();                  // lista syttyneille napeille
+        private List<int> pushedButton = new List<int>();               // lista painetuille napeille
+        private Random rnd = new Random();
+        private static System.Timers.Timer lightTimer;                  // pelin pääajastin
+        private static System.Timers.Timer redTimer;                    // nappien ajastimet
+        private static System.Timers.Timer yellowTimer;
+        private static System.Timers.Timer greenTimer;
+        private int buttonLightDelay = 750;
+        private int mainDelay = 1250;
+        private int marker = 0;                                         // painettujen nappien indeksinumerona toimiva muuttuja
+        private int points = 0;                                         // pistelaskuri
+                
+        public Spedeily()
+        {
+            InitializeComponent();
+        }
+
+        //
+        // Kaikissa seuraavissa kolmessa napissa sama toiminto, lisää elementin (napin mukaan numeroituna) pushedButton-listaan ja kasvattaa marker-muuttujaa yhdellä.
+        // Lisäksi napin väri muuttuu takaisin tummemmaksi.
+        //
+        private void redButton_Click(object sender, EventArgs e)        
+        {
+            pushedButton.Add(1);
+            marker++;
+            redButton.BackColor = Color.DarkRed;
+        }
+    
+
+        private void yellowButton_Click(object sender, EventArgs e)
+        {
+            pushedButton.Add(2);
+            marker++;
+            yellowButton.BackColor = Color.Gold;
+        }
+    
+
+        private void greenButton_Click(object sender, EventArgs e)
+        {
+            pushedButton.Add(3);
+            marker++;
+            greenButton.BackColor = Color.DarkGreen;
+        }
+
+        /*
+            Metodi sytyttää valoja satunnaisesti, ja käynnistää aina valon kohdalla ajastimen, joka Elapsed-eventillä sammuttaa valon taas. Ajastin tuottaa vain yhden eventin.
+        */
+        private void lightButton(int delay)                             
+        {
+            int lit = rnd.Next(1, 4);
+            litButton.Add(lit);
+            switch (lit)
+            {
+                case 1:
+                    {
+                        redButton.BackColor = Color.Red;
+                        redTimer = new System.Timers.Timer(delay);
+                        redTimer.Elapsed += redTimedEvent;
+                        redTimer.AutoReset = false;
+                        redTimer.Enabled = true;
+                        break;
+                    }
+                case 2:
+                    {
+                        yellowButton.BackColor = Color.Yellow;
+                        yellowTimer = new System.Timers.Timer(delay);
+                        yellowTimer.Elapsed += yellowTimedEvent;
+                        yellowTimer.AutoReset = false;
+                        yellowTimer.Enabled = true;
+                        break;
+                    }
+                case 3:
+                    {
+                        greenButton.BackColor = Color.SpringGreen;
+                        greenTimer = new System.Timers.Timer(delay);
+                        greenTimer.Elapsed += greenTimedEvent;
+                        greenTimer.AutoReset = false;
+                        greenTimer.Enabled = true;
+                        break;
+                    }
+            }
+        }
+
+        private void redTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            redButton.BackColor = Color.DarkRed;
+        }
+
+        private void yellowTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            yellowButton.BackColor = Color.Gold;
+        }
+
+        private void greenTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            greenButton.BackColor = Color.DarkGreen;
+        }
+        // 
+        // Metodi, jolla verrataan painettujen ja syttyneiden nappien listojen alkioita keskenään. Palauttaa "true", mikäli oikein (ja ylimääräisiä lyöntejä ei ole tehty), tai jos mitään 
+        // värillistä nappia ei ole vielä painettu. 
+        //
+        private bool compareLists()
+        {
+            bool match = false;
+            if (pushedButton.Count == 0)
+                match = true;
+            else if (pushedButton.Count > litButton.Count)
+                match = false;
+            else if (pushedButton[marker - 1] == litButton[marker - 1])
+                match = true;
+            return match;
+        }
+
+        /*
+            Metodi aloittaa pelin käynnistämällä ajastimen. Ajastimen aika on tällä hetkellä vielä kiinteä. Se myös nollaa listat ja marker-muuttujan aluksi varmuuden vuoksi, mikäli 
+            värinappeja on paineltu pelin olematta vielä käynnissä.
+        */
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            litButton.Clear();
+            pushedButton.Clear();
+            int delay = 1250;
+            lightTimer = new System.Timers.Timer(delay);
+            lightTimer.Elapsed += lightTimedEvent;
+            lightTimer.AutoReset = true;
+            lightTimer.Enabled = true;
+        }
+        
+        /*
+            Metodi toimii ajastimen kutsumana. Se tekee vertailun litButton- ja pushedButton-listojen välillä, jatkaa peliä ja lisää marker-muuttujaan yhden, mikäli virhelyöntiä tai 
+            ylimääräisiä lyöntejä ei ole tehty, tai litButton-listassa ei ole yli neljää alkiota enempää kuin pushedButton-listassa. Mikäli ehdot eivät täyty, lightTimer-ajastin 
+            pysäytetään, nappien värit muutetaan takaisin tummiksi, ja litButton- ja pushedButton-listat sekä marker- ja points-muuttujat nollataan. 
+        */
+        private void lightTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            if (compareLists() == true & litButton.Count - pushedButton.Count < 5)
+            {
+                points = marker;
+                lightTimer.Interval = lightTimer.Interval - 50;
+                lightButton(buttonLightDelay);
+                redTimer.Interval = redTimer.Interval - 50;
+                yellowTimer.Interval = yellowTimer.Interval - 50;
+                greenTimer.Interval = greenTimer.Interval - 50;
+                
+            }
+
+            else
+            {
+                lightTimer.Enabled = false;
+                redButton.BackColor = Color.DarkRed;
+                yellowButton.BackColor = Color.Gold;
+                greenButton.BackColor = Color.DarkGreen;
+                litButton.Clear();
+                pushedButton.Clear();
+                string message = "You got " + points + " points.";
+                string caption = "GAME OVER!";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show(message, caption, buttons);
+                marker = 0;
+                points = 0;
+            }
+        }
+
+        //
+        // Ohjelman lopetusnappi
+        //
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            lightTimer.Stop();
+            lightTimer.Dispose();
+            redTimer.Stop();
+            redTimer.Dispose();
+            yellowTimer.Stop();
+            yellowTimer.Dispose();
+            greenTimer.Stop();
+            greenTimer.Dispose();
+            Close();
+        }
+    }
+}
