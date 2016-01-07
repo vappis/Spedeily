e37ab83b426208231089154d3e2f3d60ef198f2b
Mikko Elopuro <vappis@gmail.com>
2016-01-07 07:33:25 +0200

    Second commit.

--- Spedeily/Form1.cs
+++ Spedeily/Form1.cs
@@ -15,10 +15,13 @@ namespace Spedeily
         private static System.Timers.Timer redTimer;                    // nappien ajastimet
         private static System.Timers.Timer yellowTimer;
         private static System.Timers.Timer greenTimer;
-        private int buttonLightDelay = 750;
-        private int mainDelay = 1250;
+        private int mainDelay = 1250;                                   // päälaskurin aloitusintervalli millisekunteina
+        private int buttonLightDelay = 1000;                            // nappien aloitusintervalli millisekunteina
         private int marker = 0;                                         // painettujen nappien indeksinumerona toimiva muuttuja
         private int points = 0;                                         // pistelaskuri
+        private bool redOn = false;                                     // boolean-muuttujat palaville valoille, ettei jo palavaa valoa sytytetä uudestaan (ei vielä käytössä)
+        private bool yellowOn = false;
+        private bool greenOn = false;
                 
         public Spedeily()
         {
@@ -34,6 +37,7 @@ namespace Spedeily
             pushedButton.Add(1);
             marker++;
             redButton.BackColor = Color.DarkRed;
+            redOn = false;
         }
     
 
@@ -42,6 +46,7 @@ namespace Spedeily
             pushedButton.Add(2);
             marker++;
             yellowButton.BackColor = Color.Gold;
+            yellowOn = false;
         }
     
 
@@ -50,6 +55,7 @@ namespace Spedeily
             pushedButton.Add(3);
             marker++;
             greenButton.BackColor = Color.DarkGreen;
+            greenOn = false;
         }
 
         /*
@@ -64,6 +70,7 @@ namespace Spedeily
                 case 1:
                     {
                         redButton.BackColor = Color.Red;
+                        redOn = true;
                         redTimer = new System.Timers.Timer(delay);
                         redTimer.Elapsed += redTimedEvent;
                         redTimer.AutoReset = false;
@@ -73,6 +80,7 @@ namespace Spedeily
                 case 2:
                     {
                         yellowButton.BackColor = Color.Yellow;
+                        yellowOn = true;
                         yellowTimer = new System.Timers.Timer(delay);
                         yellowTimer.Elapsed += yellowTimedEvent;
                         yellowTimer.AutoReset = false;
@@ -82,6 +90,7 @@ namespace Spedeily
                 case 3:
                     {
                         greenButton.BackColor = Color.SpringGreen;
+                        greenOn = true;
                         greenTimer = new System.Timers.Timer(delay);
                         greenTimer.Elapsed += greenTimedEvent;
                         greenTimer.AutoReset = false;
@@ -94,16 +103,19 @@ namespace Spedeily
         private void redTimedEvent(Object source, ElapsedEventArgs e)
         {
             redButton.BackColor = Color.DarkRed;
+            redOn = false;
         }
 
         private void yellowTimedEvent(Object source, ElapsedEventArgs e)
         {
             yellowButton.BackColor = Color.Gold;
+            yellowOn = false;
         }
 
         private void greenTimedEvent(Object source, ElapsedEventArgs e)
         {
             greenButton.BackColor = Color.DarkGreen;
+            greenOn = false;
         }
         // 
         // Metodi, jolla verrataan painettujen ja syttyneiden nappien listojen alkioita keskenään. Palauttaa "true", mikäli oikein (ja ylimääräisiä lyöntejä ei ole tehty), tai jos mitään 
@@ -122,15 +134,14 @@ namespace Spedeily
         }
 
         /*
-            Metodi aloittaa pelin käynnistämällä ajastimen. Ajastimen aika on tällä hetkellä vielä kiinteä. Se myös nollaa listat ja marker-muuttujan aluksi varmuuden vuoksi, mikäli 
+            Metodi aloittaa pelin käynnistämällä ajastimen. Se myös nollaa listat ja marker-muuttujan aluksi varmuuden vuoksi, mikäli 
             värinappeja on paineltu pelin olematta vielä käynnissä.
         */
         private void startButton_Click(object sender, EventArgs e)
         {
             litButton.Clear();
             pushedButton.Clear();
-            int delay = 1250;
-            lightTimer = new System.Timers.Timer(delay);
+            lightTimer = new System.Timers.Timer(mainDelay);
             lightTimer.Elapsed += lightTimedEvent;
             lightTimer.AutoReset = true;
             lightTimer.Enabled = true;
@@ -139,19 +150,23 @@ namespace Spedeily
         /*
             Metodi toimii ajastimen kutsumana. Se tekee vertailun litButton- ja pushedButton-listojen välillä, jatkaa peliä ja lisää marker-muuttujaan yhden, mikäli virhelyöntiä tai 
             ylimääräisiä lyöntejä ei ole tehty, tai litButton-listassa ei ole yli neljää alkiota enempää kuin pushedButton-listassa. Mikäli ehdot eivät täyty, lightTimer-ajastin 
-            pysäytetään, nappien värit muutetaan takaisin tummiksi, ja litButton- ja pushedButton-listat sekä marker- ja points-muuttujat nollataan. 
+            pysäytetään, nappien värit muutetaan takaisin tummiksi, ja litButton- ja pushedButton-listat sekä marker- ja points-muuttujat nollataan. Metodi myös nopeuttaa peliä jokaisella 
+            kutsulla puolen sekunnin verran.
         */
         private void lightTimedEvent(Object source, ElapsedEventArgs e)
         {
             if (compareLists() == true & litButton.Count - pushedButton.Count < 5)
             {
                 points = marker;
-                lightTimer.Interval = lightTimer.Interval - 50;
+                if (lightTimer.Interval > 100)
+                {
+                    lightTimer.Interval = lightTimer.Interval - 50;
+                }
+                if (buttonLightDelay > 100)
+                {
+                    buttonLightDelay = buttonLightDelay - 50;
+                }
                 lightButton(buttonLightDelay);
-                redTimer.Interval = redTimer.Interval - 50;
-                yellowTimer.Interval = yellowTimer.Interval - 50;
-                greenTimer.Interval = greenTimer.Interval - 50;
-                
             }
 
             else
@@ -162,6 +177,7 @@ namespace Spedeily
                 greenButton.BackColor = Color.DarkGreen;
                 litButton.Clear();
                 pushedButton.Clear();
+                buttonLightDelay = 1000;
                 string message = "You got " + points + " points.";
                 string caption = "GAME OVER!";
                 MessageBoxButtons buttons = MessageBoxButtons.OK;
